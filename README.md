e456536c7c285978e3ef4a57745d6c844cf0cd5d
Miguel Cid Y <miguelcidyevenes@gmail.com>
2023-01-29 22:56:16 -0300

    Creacion de inventario con codigo Javascript

---
+++ README.md
@@ -0,0 +1,8 @@
+## Proyecto Ecommerce Modulo 4
+
+EL proyecto consiste en la creacion de un mantenedor de inventario para un sitio E-commerce.
+
+
+## Repositorio Github
+
+ 
---
+++ assets/js/productos.js
@@ -0,0 +1,25 @@
+let productos = [
+    {
+        id: "i001",
+        nombre: "Iphone 11",
+        descripcion: "Telefono inteligente con procesador A11",
+        precio: 400000,
+        stock: 2
+    },
+    {
+        id: "i002",
+        nombre: "Iphone 12",
+        descripcion: "Telefono inteligente con procesador A12",
+        precio: 500000,
+        stock: 3
+    },
+    {
+        id: "i003",
+        nombre: "Ipgone 13",
+        descripcion: "Telefono inteligente con procesador A13",
+        precio: 350000,
+        stock: 5
+    }
+]
+
+export default productos;
